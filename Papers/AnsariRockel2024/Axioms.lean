f2e026240309d82e403b5996e6595495b6f645ee
Marcus Rockel <marcusrockel@icloud.com>
2026-09-23 17:59:46 +0200

    Verify Nelsen 2 lower-orthant parameter order

--- Papers/AnsariRockel2024/Axioms.lean
+++ Papers/AnsariRockel2024/Axioms.lean
@@ -471,6 +471,8 @@ import Verification.AxiomAudit
 #print axioms Papers.AnsariRockel2024.nelsen8_tails
 #assert_standard_axioms Papers.AnsariRockel2024.nelsen8_tails
 
+#print axioms Papers.AnsariRockel2024.nelsen2_lowerOrthant
+#assert_standard_axioms Papers.AnsariRockel2024.nelsen2_lowerOrthant
 #print axioms Papers.AnsariRockel2024.nelsen2_not_pqd
 #assert_standard_axioms Papers.AnsariRockel2024.nelsen2_not_pqd
 #print axioms Papers.AnsariRockel2024.nelsen2_not_ci
